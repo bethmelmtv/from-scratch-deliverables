ee96996d457352ce490f631e0db62a8e398eb539
Beth <bethmelmtv@gmail.com>
2022-03-08 11:51:20 -0800

    setup html elements

--- app.js
+++ app.js
@@ -6,3 +6,18 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+
+  let const yosemiteButton = document.getElementById('yosemitebutton');
+  let const hollywoodButton = document.getElementById('hollywoodbutton');
+  let const sanFranciscoButton = document.getElementById('sfbutton');
+
+//containers, we dont need to get individual pieces like each photo, because i think its not necessary
+
+  let const yosemiteContainer = getElementById('YosemiteContainer')
+  let const hollywoodContainer = getElementById('HollywoodContainer')
+  let const sanFranciscoContainer = getElementById('SanFranciscoContainer')
+
+//hiding places
+
+  let const 
